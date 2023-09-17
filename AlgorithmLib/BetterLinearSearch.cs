351ce3dec4dde76a4778f222a6ed0512b264f790
tylerenglish <eng20007@byui.edu>
2023-09-16 18:51:06 -0600

    Finished BLS

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -5,6 +5,13 @@ public static class BetterLinearSearch
     public static int Search(List<IComparable> data, IComparable target)
     {
         // ADD CODE HERE AND FIX RETURN STATEMENT
+        for (var i=0; i < data.Count; i++)
+        {
+            if (data[i].Equals(target))
+            {
+                return i;
+            }
+        }
         return -1;
     }
 }
